b3b269ebe89fa7c8e84841d2e26db9e5c32ea4f1
Alex Dzyoba <alex@dzyoba.com>
2018-07-27 15:17:55 +0300

    Add time formatting via flag

--- t.go
+++ t.go
@@ -4,14 +4,57 @@ import (
 	"fmt"
 	"os"
 	"time"
+
+	"github.com/spf13/pflag"
 )
 
 func usage() {
 	fmt.Printf("Usage: %s <target timezone> [source time] [source timezone]\n", os.Args[0])
 }
 
+func formatSelect(flag string) string {
+	switch flag {
+	case "u":
+		fallthrough
+	case "unix":
+		return time.UnixDate
+
+	case "1123":
+		fallthrough
+	case "rfc1123":
+		fallthrough
+	case "RFC1123":
+		return time.RFC1123
+
+	case "3339":
+		fallthrough
+	case "rfc3339":
+		fallthrough
+	case "RFC3339":
+		return time.RFC3339
+
+	case "t":
+		fallthrough
+	case "time":
+		return "15:04"
+
+	case "t2":
+		fallthrough
+	case "time2":
+		fallthrough
+	case "kitchen":
+		return time.Kitchen
+
+	default:
+		return flag
+	}
+}
+
 func main() {
-	argc := len(os.Args)
+	formatFlag := pflag.StringP("format", "f", "time", "Time format (time, unix, rfc1123, rfc3339, kitchen)")
+	pflag.Parse()
+
+	argc := len(pflag.Args())
 
 	sourceTime := time.Now()
 	sourceLocation := time.Local
@@ -19,15 +62,15 @@ func main() {
 
 	var err error
 	switch argc {
-	case 4: //tzconv <target timezone> <source time> <source timezone>
-		sourceLocation, err = time.LoadLocation(os.Args[3])
+	case 3: //tzconv <target timezone> <source time> <source timezone>
+		sourceLocation, err = time.LoadLocation(pflag.Arg(2))
 		if err != nil {
 			panic(err)
 		}
 		fallthrough // to handle source time and target timezone
 
-	case 3: //tzconv <target tz> <source time>
-		parsedTime, err := time.Parse("15:04", os.Args[2])
+	case 2: //tzconv <target tz> <source time>
+		parsedTime, err := time.Parse("15:04", pflag.Arg(1))
 		if err != nil {
 			panic(err)
 		}
@@ -37,13 +80,13 @@ func main() {
 		sourceTime = time.Date(now.Year(), now.Month(), now.Day(), parsedTime.Hour(), parsedTime.Minute(), parsedTime.Second(), 0, sourceLocation)
 		fallthrough // to handle target timezone
 
-	case 2: // tzconv <target tz>
-		targetLocation, err = time.LoadLocation(os.Args[1])
+	case 1: // tzconv <target tz>
+		targetLocation, err = time.LoadLocation(pflag.Arg(0))
 		if err != nil {
 			panic(err)
 		}
 
-	case 1: // tzconv
+	case 0: // tzconv
 		// just print the current time
 
 	default:
@@ -52,11 +95,7 @@ func main() {
 	}
 
 	targetTime := sourceTime.In(targetLocation)
-	fmt.Println(targetTime.Format(time.UnixDate))
-	fmt.Println(targetTime.Format(time.RFC1123))
-	fmt.Println(targetTime.Format(time.RFC3339))
-	fmt.Println(targetTime.Format(time.RFC822))
-	fmt.Println(targetTime.Format(time.RFC850))
-	fmt.Println(targetTime.Format(time.ANSIC))
-	fmt.Println(targetTime.Format(time.Kitchen))
+
+	format := formatSelect(*formatFlag)
+	fmt.Println(targetTime.Format(format))
 }
